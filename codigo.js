0fe3d439053a74f1ff0112c104e18be76bbd6225
Tania024 <84852408+Tania024@users.noreply.github.com>
2024-04-16 22:39:23 -0500

    proyecto completado
    dibujar los botones con js

--- codigo.js
+++ codigo.js
@@ -1,5 +1,52 @@
 let displayValue = '';
 
+function drawNumbers() {
+    const numbersContainer = document.querySelector('.button-container');
+
+    for (let i = 0; i <= 9; i++) {
+        const button = document.createElement('button');
+        button.textContent = i;
+        button.classList.add('calculator-button', 'orange');
+        button.addEventListener('click', () => appendToDisplay(i.toString()));
+        numbersContainer.appendChild(button);
+    }
+    
+    const zeroButton = document.createElement('button');
+    zeroButton.textContent = '0';
+    zeroButton.classList.add('calculator-button', 'orange');
+    zeroButton.addEventListener
+}
+
+drawNumbers();
+
+function drawOperators() {
+    const operatorsContainer = document.querySelector('.button-container');
+
+    const operators = ['+', '-', '*', '/', '=','C','Borrar'];
+
+    operators.forEach(operator => {
+        const button = document.createElement('button');
+        button.textContent = operator;
+        button.classList.add('calculator-button', 'green');
+        button.addEventListener('click', () => {
+            if (operator === '=') {
+                Resultado();
+            } else if (operator === 'C') {
+                clearDisplay();
+            } else if (operator === 'Borrar') {
+                eliminar();
+            } else {
+                addToDisplay(operator);
+            }
+        });
+        operatorsContainer.appendChild(button);
+    });
+}
+
+drawOperators();
+
+
+
 function appendToDisplay(digit) {
     displayValue += digit;
     updateDisplay();
@@ -36,20 +83,3 @@ function updateDisplay() {
 
 const buttons = document.querySelectorAll('.calculator-button');
 
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        const buttonText = button.textContent;
-        
-        if (!isNaN(parseInt(buttonText)) || buttonText === '.') {
-            appendToDisplay(buttonText);
-        } else if (buttonText === 'C') {
-            clearDisplay();
-        } else if (buttonText === '=') {
-            Resultado();
-        } else if (buttonText === 'Borrar') {
-            eliminar();
-        } else {
-            addToDisplay(buttonText);
-        }
-    });
-});
